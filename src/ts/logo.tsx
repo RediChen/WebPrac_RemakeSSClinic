d07d362f52f21bce2ce9cdc02d392d1a5884f8fe
RediiChen <a0929052958@gmail.com>
2021-08-19 18:44:03 +0800

    修正無法匯入圖檔的問題
    使用新套件 file-loader，加上設定檔

--- src/ts/logo.tsx
+++ src/ts/logo.tsx
@@ -1,9 +1,26 @@
-
-export default function Logo(props: {size : string}) {
-    //input e.g. : size=64 (in px)
-    const address = "/favicon/" + props.size + "px_woFrame.png";
+import Logo32px from "src/assets/favicon/woFrame32px.png";
+import Logo64px from "src/assets/favicon/woFrame64px.png";
+import Logo128px from "src/assets/favicon/woFrame128px.png";
+export default function Logo(props: { size: string }) {
+    //size=32, 64, 128 (in px)
+    let address: string;
+    switch (props.size) {
+        case "32":
+            address = Logo32px;
+            break;
+        case "64":
+            address = Logo64px;
+            break;
+        case "128":
+            address = Logo128px;
+            break;
+        default:
+            address = "";
+            console.error("輸入的 " + props.size + " px 是未供應的 Logo 尺寸。");
+    }
+    //src={`Logo${props.size}px`}
     return (
-        <img className={`logo logo-${props.size}`} src={(address)}
+        <img className={`logo logo-${props.size}`} src={address}
             alt="心晴診所 Sunshine Clinic logo" />
     );
 }
