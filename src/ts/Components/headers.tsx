557363ddc71ac1859ee6d4fb0fce2428b6658a4c
RediiChen <a0929052958@gmail.com>
2021-08-24 20:04:23 +0800

    新增 H3 元件

--- src/ts/Components/headers.tsx
+++ src/ts/Components/headers.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 interface IHeaderProps {
-    text : string
+    text: string
 }
-const H1: React.FC<IHeaderProps> = ({text}) =>
+const H1: React.FC<IHeaderProps> = ({ text }) =>
     <header>
         <h1 className="title-1">{text}</h1>
         <hr className="hr-1" />
@@ -12,4 +12,12 @@ const H2: React.FC<IHeaderProps> = ({ text }) =>
         <h3 className="title-2">{text}</h3>
         <hr className="hr-3" />
     </>
-export {H1, H2};
+const H3: React.FC<IHeaderProps> = ({ text }) =>
+    <>
+        <h3>{text}</h3>
+        <div className="hr-2">
+            <div></div>
+            <hr />
+        </div>
+    </>
+export { H1, H2, H3 };
